9f0320ce5e6eacf21e8e1bde1dcbc35f543961ed
shubhamkumar96 <shubhamkumarranu@gmail.com>
2022-05-31 02:31:53 +0530

    Added PUT Endpoint for Updating Note

--- src/main/java/com/notes/notetakingapp/controllers/NoteController.java
+++ src/main/java/com/notes/notetakingapp/controllers/NoteController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -56,5 +57,30 @@ public class NoteController {
             return note;
         }
         return "No note with the given noteId found";
+    }
+
+    @PutMapping("/updatenote")
+    public List<Note> updateNote(@RequestBody Note newNote) {
+        List<Note> oldAndNewNotes = new ArrayList<>();
+
+        //Update to current time
+        newNote.setDateTime(ZonedDateTime.ofInstant(new Date().toInstant(), ZoneId.systemDefault()));
+
+        Long noteId = newNote.getId();
+        Optional<Note> noteOp = noteRepository.findById(noteId);
+        // If there is note with given noteId present, then we will update it
+        if(noteOp.isPresent()) {
+            Note oldNote = noteOp.get();
+            oldAndNewNotes.add(new Note(oldNote.getId(), "[OLD] " + oldNote.getTitle(), oldNote.getBody(), oldNote.getDateTime())); // Add the old note
+            oldNote.setBody(newNote.getBody());
+            oldNote.setTitle(newNote.getTitle());
+            oldNote.setDateTime(newNote.getDateTime());
+            noteRepository.save(oldNote);
+        } else {    // Else we will save the new note to the database
+            noteRepository.save(newNote);
+        }
+
+        oldAndNewNotes.add(newNote);
+        return oldAndNewNotes;
     }
 }
